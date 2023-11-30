3911530d142334e77e4b44ec1b5523dc965d6bbb
dyprodg <dennisdiepolder@outlook.com>
2023-11-30 16:02:01 +0100

    removefunction

--- index.js
+++ index.js
@@ -3,31 +3,33 @@ const s3 = new AWS.S3();
 
 exports.handler = async (event, context) => {
     try {
-        // Informationen über das hochgeladene Objekt
         const bucket = event.Records[0].s3.bucket.name;
         const key = event.Records[0].s3.object.key;
 
-        // Überprüfen, ob die Datei eine .txt-Datei ist
-        if (key.toLowerCase().endsWith('.txt')) {
-            // Download der Datei vom Quell-Bucket
-            const data = await s3.getObject({ Bucket: bucket, Key: key }).promise();
-            const content = data.Body.toString();
+        if (event.Records[0].eventName.startsWith('ObjectCreated')) {
+            // Datei wurde im Quell-Bucket erstellt
 
-            // Zählen der Großbuchstaben in der Datei
-            const uppercaseCount = (content.match(/[A-Z]/g) || []).length;
+            if (key.toLowerCase().endsWith('.txt')) {
+                const data = await s3.getObject({ Bucket: bucket, Key: key }).promise();
+                const content = data.Body.toString();
 
-            // Transformation der Daten
-            const transformedContent = content.toLowerCase();
+                const uppercaseCount = (content.match(/[A-Z]/g) || []).length;
+                const transformedContent = content.toLowerCase();
 
-            // Datei im Ziel-Bucket speichern
-            const targetKey = `${key.split('.')[0]}_${uppercaseCount}.txt`;
-            await s3.upload({ Bucket: 'dennis-ha-target', Key: targetKey, Body: transformedContent }).promise();
+                const targetKey = `${key.split('.')[0]}_${uppercaseCount}.txt`;
+                await s3.upload({ Bucket: 'dennis-ha-target', Key: targetKey, Body: transformedContent }).promise();
 
-            // Konsolenlog für Erfolg
-            console.log(`Datei "${key}" erfolgreich verarbeitet und in "dennis-ha-target" Bucket hochgeladen.`);
-        } else {
-            // Konsolenlog, wenn die Datei keine .txt-Datei ist
-            console.log(`Die hochgeladene Datei "${key}" ist keine Textdatei. Die Funktion wird abgebrochen.`);
+                console.log(`Datei "${key}" erfolgreich verarbeitet und in "dennis-ha-target" Bucket hochgeladen.`);
+            } else {
+                console.log(`Die hochgeladene Datei "${key}" ist keine Textdatei. Die Funktion wird abgebrochen.`);
+            }
+        } else if (event.Records[0].eventName.startsWith('ObjectRemoved')) {
+            // Datei wurde im Quell-Bucket gelöscht
+
+            const targetKey = `${key.split('.')[0]}_`; // Anpassen, um die korrekte Ziel-Datei zu identifizieren
+            await s3.deleteObject({ Bucket: 'dennis-ha-target', Key: targetKey }).promise();
+
+            console.log(`Datei "${key}" erfolgreich gelöscht im "dennis-ha-target" Bucket.`);
         }
 
         return {
